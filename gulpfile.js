32a16d859944f67e357f56729f087855c237877a
Ariel Faur <ariel.faur@gmail.com>
2016-04-24 15:58:27 -0300

    update license and add gulp tasks for minification

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+gulp.task('compress', function() {
+  gulp.src('src/ion-pullup.js')
+    .pipe(gulp.dest('dist'))
+    .pipe(uglify())  
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['compress']);
