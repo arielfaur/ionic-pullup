3a306f4c76c405d5225525595938a89c37546300
Ariel Faur <ariel.faur@gmail.com>
2016-04-25 19:34:10 -0300

    added deployment to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var rename = require('gulp-rename');
 gulp.task('compress', function() {
   gulp.src('src/ion-pullup.js')
     .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('example/www/js'))
     .pipe(uglify())  
     .pipe(rename({extname: '.min.js'}))
     .pipe(gulp.dest('dist'));
