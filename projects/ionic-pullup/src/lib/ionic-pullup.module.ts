3af94d9db7317bd06a2dbfb597dd6a9bab77b94c
Ariel Faur <ariel.faur@gmail.com>
2020-10-06 11:32:08 -0300

    upgrade to angular 9

--- projects/ionic-pullup/src/lib/ionic-pullup.module.ts
+++ projects/ionic-pullup/src/lib/ionic-pullup.module.ts
@@ -1,9 +1,10 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, Injectable } from '@angular/core';
 import { IonicPullupComponent } from './ionic-pullup.component';
 import { IonicPullupComponentTabComponent } from './ionic-pullup-tab.component';
 import * as Hammer from 'hammerjs';
 import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 
+@Injectable()
 export class MyHammerConfig extends HammerGestureConfig {
   overrides = {
     pan: { direction: Hammer.DIRECTION_VERTICAL },
